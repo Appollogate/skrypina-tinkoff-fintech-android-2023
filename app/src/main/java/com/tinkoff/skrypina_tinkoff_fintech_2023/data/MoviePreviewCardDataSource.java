a15f6192ff7f9aaec5febae9f376b111a258093c
Daria Skrypina <dskrypina@gmail.com>
2023-02-05 15:39:14 +0300

    Added http request handler

--- app/src/main/java/com/tinkoff/skrypina_tinkoff_fintech_2023/data/MoviePreviewCardDataSource.java
+++ app/src/main/java/com/tinkoff/skrypina_tinkoff_fintech_2023/data/MoviePreviewCardDataSource.java
@@ -1,11 +1,14 @@
 package com.tinkoff.skrypina_tinkoff_fintech_2023.data;
 
 import com.tinkoff.skrypina_tinkoff_fintech_2023.model.MoviePreviewCard;
+import com.tinkoff.skrypina_tinkoff_fintech_2023.net.HttpRequestHandler;
 
 import java.util.List;
 
 public class MoviePreviewCardDataSource {
     public List<MoviePreviewCard> loadMoviePreviewCards() {
+        String top100FilmsJSON = new HttpRequestHandler().getTop100FilmsJSON();
+
         // TODO: pull data from Kinopoisk servers
         return List.of(
                 new MoviePreviewCard("", "A", "a", "aa"),
---
+++ app/src/main/java/com/tinkoff/skrypina_tinkoff_fintech_2023/net/HttpRequestHandler.java
@@ -0,0 +1,58 @@
+package com.tinkoff.skrypina_tinkoff_fintech_2023.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HttpRequestHandler {
+
+    static final String TOP_100_FILMS_URL = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS";
+    static final String HEADER_KEY_NAME = "X-API-KEY";
+    static final String HEADER_KEY_VALUE = "e30ffed0-76ab-4dd6-b41f-4c9da2b2735b";
+    static final String HEADER_ACCEPT_KEY = "accept";
+    static final String HEADER_ACCEPT_VALUE = "application/json";
+
+    public String getTop100FilmsJSON() {
+        URL url;
+        try {
+            url = new URL(TOP_100_FILMS_URL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("URL " + TOP_100_FILMS_URL + "is malformed.\n");
+        }
+
+        HttpURLConnection conn;
+        try {
+            conn = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Couldn't open connection to " + TOP_100_FILMS_URL);
+        }
+
+        // Set headers
+        conn.setRequestProperty(HEADER_ACCEPT_KEY, HEADER_ACCEPT_VALUE);
+        conn.setRequestProperty(HEADER_KEY_NAME, HEADER_KEY_VALUE);
+        try {
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new RuntimeException("Request failed. HTTP error code: " + conn.getResponseCode());
+            }
+            // Read response
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder jsonString = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                jsonString.append(line);
+            }
+            br.close();
+            conn.disconnect();
+            return jsonString.toString();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Couldn't read response from server.\n");
+        }
+    }
+}
